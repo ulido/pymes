ae43114f7e339d570ee33446ea08db8dfe665155
Ulrich Dobramysl <ulido@users.noreply.github.com>
2023-09-05 14:28:21 +0100

    Code formatting

--- src/pypop/pypop.py
+++ src/pypop/pypop.py
@@ -16,20 +16,22 @@ __all__ = [
     "PredationBirthReaction",
 ]
 
+
 class Species:
     """Defines a species."""
+
     def __init__(self, name: str):
         """Initialize a new species. Takes the `name` of the species as an argument (needs to be unique!)."""
         self.name: str = name
         self.members: list[Occupant] = []
         self._hash = hash(name)
-    
+
     def add(self, occupant: Occupant):
         """Add a new `occupant` to this species."""
-        assert(occupant.species == self)
+        assert occupant.species == self
         occupant._species_index: int = len(self.members)
         self.members.append(occupant)
-    
+
     def remove(self, occupant):
         """Remove an `occupant` from this species (i.e. the occupant dies)."""
         index: int = occupant._species_index
@@ -40,26 +42,30 @@ class Species:
 
     def __hash__(self):
         return self._hash
-    
+
     def __len__(self):
         return len(self.members)
 
     def __str__(self):
         return self.name
 
+
 class Site:
     """Describes a lattice site."""
+
     def __init__(self, id):
         """Initialize a new lattice site with a unique `id`."""
         self.id = id
         self.species_occupants: dict[Species, list[Occupant]] = defaultdict(list)
         self.neighbors: list[Site] = []
-    
+
     def species_abundance(self, species: Species) -> int:
         """Returns the abundance of the given `species` at this lattice site."""
         return len(self.species_occupants[species])
 
-    def get_random_occupant(self, species: Species, rng: np.random.Generator) -> Occupant:
+    def get_random_occupant(
+        self, species: Species, rng: np.random.Generator
+    ) -> Occupant:
         """Return a random occupant of the given `species`. The random number generator used needs to be given as `rng`."""
         occupant_list = self.species_occupants[species]
         number = len(occupant_list)
@@ -85,10 +91,14 @@ class Site:
     def __str__(self):
         return str(self.id)
 
+
 # Internal counter to give unique IDs to occupants.
 occupant_counter = itertools.count()
+
+
 class Occupant:
     """Describes a lattice site occupant."""
+
     def __init__(self, species: Species, initial_site: Site):
         """Initialize a new site occupant of the given `species` at the `initial_site`."""
         self.id = next(occupant_counter)
@@ -97,7 +107,7 @@ class Occupant:
         self.species.add(self)
         self.site = initial_site
         self.site.add(self)
-    
+
     def destroy(self):
         """Destroys the occupant. Removes it from its current lattice site and from the global species list."""
         self.species.remove(self)
@@ -115,14 +125,18 @@ class Occupant:
     def __str__(self):
         return f"{self.species.name}{self.id}"
 
+
 class Lattice:
     """Describes a 2D lattice."""
+
     def __init__(self, size: tuple[int, int]):
         """Initialize a lattice with the given `size` (e.g. 256x256 sites)."""
         self.size: tuple[int, int] = size
         self.nr_sites = self.size[0] * self.size[1]
         # Linear list of sites, where the site IDs are given as the xy index coordinates.
-        self.sites: list[Site] = [Site(f"{i}x{j}") for i in range(size[0]) for j in range(size[1])]
+        self.sites: list[Site] = [
+            Site(f"{i}x{j}") for i in range(size[0]) for j in range(size[1])
+        ]
         i: int
         site: Site
         # Iterate over all sites and assign neighbor sites.
@@ -133,26 +147,28 @@ class Lattice:
                 site.neighbors.append(self.sites[i + size[0] - 1])
             else:
                 site.neighbors.append(self.sites[i - 1])
-            if x == size[0]-1:
+            if x == size[0] - 1:
                 site.neighbors.append(self.sites[i - size[0] + 1])
             else:
                 site.neighbors.append(self.sites[i + 1])
-            
+
             if y == 0:
-                site.neighbors.append(self.sites[size[0]*(size[1]-1) + x])
+                site.neighbors.append(self.sites[size[0] * (size[1] - 1) + x])
             else:
-                site.neighbors.append(self.sites[(y-1)*size[0]+x])
-            if y == size[1]-1:
+                site.neighbors.append(self.sites[(y - 1) * size[0] + x])
+            if y == size[1] - 1:
                 site.neighbors.append(self.sites[x])
             else:
-                site.neighbors.append(self.sites[(y+1)*size[0]+x])
+                site.neighbors.append(self.sites[(y + 1) * size[0] + x])
+
 
 class Reaction:
     """Base class describing a reaction. Has to be subclassed."""
+
     def __init__(self, rate: float):
         """Initialize reaction with the given `rate`."""
         self.rate = rate
-    
+
     def decide(self, rng: np.random.Generator):
         """Decide if the reaction takes place by generating a random number from the generator `rng`."""
         return (self.rate >= 1.0) or (rng.random() < self.rate)
@@ -161,26 +177,30 @@ class Reaction:
         """Decides if reaction will be performed and if yes performs the reaction - needs to be overridden by the subclass' method. Needs to return `True` if the `occupant` was destroyed."""
         raise NotImplementedError
 
+
 class BirthReaction(Reaction):
     """Describes an occupant birth reaction (A->2A)."""
+
     def __init__(self, species: Species, rate: float):
         """Initialize a birth reaction of `species` with the given `rate`."""
         self.species: Species = species
         super().__init__(rate)
-    
+
     def __call__(self, occupant: Occupant, rng: np.random.Generator) -> bool:
         """Decides if birth reaction occurs for the given `occupant` (using the random generator `rng`) and if yes, generates a new occupant of the same species at the `occupant`'s site. Returns `False` because the `occupant` will never be destroyed in a birth reaction."""
         if self.decide(rng):
             new_occupant = Occupant(occupant.species, occupant.site)
         return False
 
+
 class DeathReaction(Reaction):
     """Describes an occupant death reaction (A->0)."""
+
     def __init__(self, species: Species, rate: float):
         """Initialize a death reaction of `species` with the given `rate`."""
         self.species: Species = species
         super().__init__(rate)
-    
+
     def __call__(self, occupant: Occupant, rng: np.random.Generator) -> bool:
         """Decides if death reaction occurs for the given `occupant` (using the random generator `rng`) and if yes, destroys the `occupant`. Returns `True` if the `occupant` was destroyed, `False` otherwise."""
         if self.decide(rng):
@@ -188,14 +208,16 @@ class DeathReaction(Reaction):
             return True
         return False
 
+
 class PredationReaction(Reaction):
     """Describes an occupant predation reaction (A+B->A)."""
+
     def __init__(self, speciesA: Species, speciesB: Species, rate: float):
         """Initialize a predation reaction of `speciesA` (predator) and `speciesB` (prey) with the given `rate`."""
         self.speciesA: Species = speciesA
         self.speciesB: Species = speciesB
         super().__init__(rate)
-    
+
     def __call__(self, occupant: Occupant, rng: np.random.Generator) -> bool:
         """For each victim of `speciesB` on the `occupant`'s lattice site, decides if predation reaction occurs. If yes, destroys the `speciesB` victim. Returns `False` because the predation reaction never destroys the original `occupant`."""
         site: Site = occupant.site
@@ -209,15 +231,17 @@ class PredationReaction(Reaction):
             victim.destroy()
 
         return False
-        
+
+
 class PredationBirthReaction(Reaction):
     """Describes an occupant predation-birth reaction (A+B->2A)."""
+
     def __init__(self, speciesA: Species, speciesB: Species, rate: float):
         """Initialize a predation-birth reaction of `speciesA` (predator) and `speciesB` (prey) with the given `rate`."""
         self.speciesA: Species = speciesA
         self.speciesB: Species = speciesB
         super().__init__(rate)
-    
+
     def __call__(self, occupant: Occupant, rng: np.random.Generator) -> bool:
         """For each victim of `speciesB` on the `occupant`'s lattice site, decides if predation reaction occurs. If yes, destroys the `speciesB` victim and creates a new occupant of `speciesA`. Returns `False` because the predation reaction never destroys the original `occupant`."""
         site: Site = occupant.site
@@ -230,30 +254,43 @@ class PredationBirthReaction(Reaction):
         for victim in to_destroy:
             victim.destroy()
             new_occupant = Occupant(occupant.species, site)
-        
+
         return False
+
 
 class Hop(Reaction):
     """Describes an occupant hop reaction between sites."""
+
     def __init__(self, species: Species, rate: float):
         """Initialize a hop reaction of the given `species` and with the given `rate`."""
         self.species: Species = species
         super().__init__(rate)
-    
+
     def __call__(self, occupant: Occupant, destination_index):
         """Hop the given `occupant` from its original site to the site with neighbor index `destination_index` (between 0 and 3 inclusive)."""
         destination: Site = occupant.site.neighbors[destination_index]
         occupant.set_site(destination)
 
+
 class World:
     """Describes the world in which the simulation takes place."""
-    def __init__(self, size: tuple[int, int], initial_densities: dict[Species, float], hops: dict[Species, Hop], reactions: dict[Species, list[Reaction]], seed=None):
+
+    def __init__(
+        self,
+        size: tuple[int, int],
+        initial_densities: dict[Species, float],
+        hops: dict[Species, Hop],
+        reactions: dict[Species, list[Reaction]],
+        seed=None,
+    ):
         """Initialize a world of the given `size` (e.g. 256x256), with the given `initial_densities`, hop reactions `hops` and inter-occupant `reactions`. Optionally the initial random `seed` can be specified."""
         self.lattice: Lattice = Lattice(size)
         density_species = set(initial_densities.keys())
         hop_species = set(hops.keys())
         reaction_species = set(reactions.keys())
-        self.species: list[Species] = list(set.union(density_species, hop_species, reaction_species))
+        self.species: list[Species] = list(
+            set.union(density_species, hop_species, reaction_species)
+        )
 
         self.hops: dict[Species, Hop] = hops
         self.reactions: dict[Species, Reaction] = reactions
@@ -288,14 +325,14 @@ class World:
         species: Species = occupant.species
         if species in self.hops:
             self.hops[species](occupant, self.random_generator.integers(4))
-        
+
         # Perform all reactions for the given occupant
         reaction: Reaction
         for reaction in self.reactions[species]:
             # If the occupant is destroyed during a reaction, exit the loop.
             if reaction(occupant, self.random_generator):
                 break
-    
+
     def step(self):
         """Perform one Monte Carlo step. A step is defined as performing N iterations where N is the current number of occupants on the lattice."""
         members_per_species = [len(species) for species in self.species]
@@ -309,7 +346,7 @@ class World:
             progress_wrap = lambda iterator: iterator
         for _ in progress_wrap(range(number_steps)):
             self.step()
-    
+
     @property
     def abundances(self) -> dict[Species, int]:
         """The abundance of all species occupants on the lattice."""
@@ -317,5 +354,9 @@ class World:
 
     def asarrays(self) -> dict[str, npt.ArrayLike]:
         """Return the current abundances of all species on each lattice site as arrays."""
-        return {species.name: np.array([site.species_abundance(species) for site in self.lattice.sites]).reshape(self.lattice.size)
-                for species in self.species}
+        return {
+            species.name: np.array(
+                [site.species_abundance(species) for site in self.lattice.sites]
+            ).reshape(self.lattice.size)
+            for species in self.species
+        }
